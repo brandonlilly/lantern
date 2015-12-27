47bf47ccd023e6d82f189d23b3e5bfb87aa8956a
michael-butler <mcbfm5gm@gmail.com>
2015-12-27 17:53:27 -0600

    initial

---
+++ lib/fixnum.rb
@@ -0,0 +1,63 @@
+# altered Fixnum class
+class Fixnum
+
+  # alias functions
+  alias_method :add,  :+
+  alias_method :sub,  :-
+  alias_method :mult, :*
+  alias_method :div,  :/
+  alias_method :modu, :%
+  alias_method :pow,  :**
+
+  # implement functions
+  def +(arg)
+    if arg.is_a?(DC)
+      return arg + self
+    end
+    self.add(arg)
+  end
+
+  def -(arg)
+    if arg.is_a?(DC)
+      return -arg + self
+    end
+    self.sub(arg)
+  end
+
+  def *(arg)
+    if arg.is_a?(DC)
+      return arg * self
+    end
+    self.mult(arg)
+  end
+
+  def /(arg)
+    if arg.is_a?(DC)
+      dc = DC.new;
+      return [
+        dc << self,
+        dc / arg,
+      ]
+    end
+    self.div(arg)
+  end
+
+  def %(arg)
+    if arg.is_a?(DC)
+      dc = DC.new;
+      return [
+        dc << self,
+        dc % arg,
+      ]
+    end
+    self.modu(arg)
+  end
+
+  def **(arg)
+    if arg.is_a?(DC)
+      abort("EXPONENTIAL NOT IMPLEMENTED")
+    end
+    self.pow(arg)
+  end
+
+end
