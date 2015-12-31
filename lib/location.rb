ee1f82bd3f4eab157a651b49c6661e36da546d27
Brandon Lilly <brandonjlilly@gmail.com>
2015-12-31 03:12:59 -0700

    add location class

---
+++ lib/location.rb
@@ -0,0 +1,30 @@
+class Location
+  attr_accessor :name
+
+  def initialize(options = {})
+    self.name = options[:name] || raise ArgumentError
+  end
+
+  def acquire(*locations)
+    locations.map do |location|
+      moveLocation(location, "Neutral", "Map Revealer", self)
+    end
+  end
+
+  def centerOn(player, unit, location)
+    moveLocation(self, player, unit, location)
+  end
+
+  def centerView
+    centerView(self)
+  end
+
+  def ping
+    ping(self)
+  end
+
+  def explode(owner = "Player 8")
+    unit = "Terran Wraith"
+    [ createUnit(owner, unit, 1, self), killUnit(owner, unit) ]
+  end
+end
